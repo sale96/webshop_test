1494bf966573af210515c30e5a95a93910289139
Aleksandar <aca801@gmail.com>
2019-09-08 12:53:59 +0200

    Created Core, Controller, Database classes. Also created config file

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+define('URL_ROOT', 'http://localhost/stuntcoders/');
+define('FILE_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/stuntcoders/');
+
---
+++ lib/Database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sale
+ * Date: 9/8/2019
+ * Time: 12:50 PM
+ */
+
+class Database
+{
+
+}
