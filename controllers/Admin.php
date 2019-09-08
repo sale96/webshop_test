e7ca6cd6a712d5828a91b69e88821af831684376
Aleksandar <aca801@gmail.com>
2019-09-08 14:19:04 +0200

    Started working on admin logic

---
+++ controllers/Admin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sale
+ * Date: 9/8/2019
+ * Time: 2:17 PM
+ */
+
+class Admin extends Controller
+{
+
+    public function index(){
+        $this->view('admin/index');
+    }
+
+}
